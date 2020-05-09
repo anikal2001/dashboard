9116f21b5fe0ad1e00bcfa2852e98d25cec40130
anikal2001 <anikal2001@gmail.com>
2020-05-09 14:51:02 -0400

    Fixed Deploy

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 var express = require('express');
-var firebase = require("firebase/app");
 var serveStatic = require('serve-static');
 var path = require('path');
 app = express();
@@ -18,21 +17,6 @@ app.use(
 );
 
 
-// Add the Firebase products that you want to use
-require("firebase/auth");
-
-const firebaseConfig = {
-    apiKey: "AIzaSyBd63jpzF1J2dErNMC_yW-eSTjw2uOnhF4",
-    authDomain: "terminal-prototype.firebaseapp.com",
-    databaseURL: "https://terminal-prototype.firebaseio.com",
-    projectId: "terminal-prototype",
-    storageBucket: "terminal-prototype.appspot.com",
-    messagingSenderId: "354894608266",
-    appId: "1:354894608266:web:b76df36fe732cae19f7a73",
-    measurementId: "G-F12RQDHE5J"
-  };
-
-
 
 
 app.use(serveStatic(__dirname + "/dist"));
@@ -40,7 +24,3 @@ var port = process.env.PORT || 8080;
 app.listen(port);
 console.log('server started '+ port);
 var hostname = '127.0.0.1';
-
-app.listen(port, hostname, () => {
-   console.log(`Server running at http://${hostname}:${port}/`);
- });
