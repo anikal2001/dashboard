bcabed5f553ec8849b69d2a4659c572d7f4081ab
anikal2001 <anikal2001@gmail.com>
2020-05-09 15:34:23 -0400

    Added Page Title

--- server.js
+++ server.js
@@ -20,6 +20,9 @@ app.use(
 
 
 app.use(serveStatic(__dirname + "/dist"));
+app.get(/.*/, function (req, res) {
+	res.sendFile(path.join(__dirname, '/dist/index.html'))
+})
 var port = process.env.PORT || 8080;
 app.listen(port);
 console.log('server started '+ port);
